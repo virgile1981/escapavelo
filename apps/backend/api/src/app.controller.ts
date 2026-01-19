16736bfce128c33a66c6a943af20e413c68ba7c6
Virgile <bourse.virgile@gmail.com>
2026-01-19 13:35:22 +0100

    fix: remove debug logging for database parameters in AppController

--- apps/backend/api/src/app.controller.ts
+++ apps/backend/api/src/app.controller.ts
@@ -4,12 +4,6 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-    console.log('BD parameters:', {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      database: process.env.DB_NAME,
-    });
   }
 
 }
